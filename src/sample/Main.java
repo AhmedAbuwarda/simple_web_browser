1d0296dd203f50f057791fa0bb07b6ac1ba04043
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2020-05-04 01:10:24 +0300

    Update Main.java file

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -20,7 +20,29 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     /**
-     * @param primaryStage stage
+     * @param args the command line here.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here.
+
+        launch(args);
+
+    }
+
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     *                     the application scene can be set. The primary stage will be embedded in
+     *                     the browser if the application was launched as an applet.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages and will not be embedded in the browser.
      * @throws Exception exception errors
      */
     @Override
@@ -31,16 +53,6 @@ public class Main extends Application {
         primaryStage.getIcons().add(new Image("firefox.png"));
         primaryStage.setScene(new Scene(root, 1220, 600));
         primaryStage.show();
-
-    }
-
-    /**
-     * @param args the command line here.
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here.
-
-        launch(args);
 
     }
 }
